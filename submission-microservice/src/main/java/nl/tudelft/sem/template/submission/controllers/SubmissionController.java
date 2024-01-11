78e99b7f73c44dd2fe7cd7a7bf90f3564aad05e8
Oleh Grypas <o.grypas@student.tudelft.nl>
2024-01-12 00:09:40 +0100

    .yaml file + SubmissionService changes

--- submission-microservice/src/main/java/nl/tudelft/sem/template/submission/controllers/SubmissionController.java
+++ submission-microservice/src/main/java/nl/tudelft/sem/template/submission/controllers/SubmissionController.java
@@ -1,8 +1,10 @@
 package nl.tudelft.sem.template.submission.controllers;
 
 import nl.tudelft.sem.template.api.SubmissionApi;
+import nl.tudelft.sem.template.model.ReturnedSubmission;
 import nl.tudelft.sem.template.model.Submission;
 import nl.tudelft.sem.template.model.SubmissionStatus;
+import nl.tudelft.sem.template.model.UpdateSubmission;
 import nl.tudelft.sem.template.submission.repositories.SubmissionRepository;
 import nl.tudelft.sem.template.submission.services.SubmissionService;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -39,7 +41,7 @@ public class SubmissionController implements SubmissionApi {
      * @return response with created submission if success, otherwise error
      */
     @Override
-    public ResponseEntity<Submission> addSubmission(Submission submission) {
+    public ResponseEntity<ReturnedSubmission> addSubmission(Submission submission) {
         return submissionService.add(submission);
     }
 
@@ -61,7 +63,7 @@ public class SubmissionController implements SubmissionApi {
      * @return submission if it is found for a given id, error otherwise
      */
     @Override
-    public ResponseEntity<Submission> getSubmissionById(UUID submissionId) {
+    public ResponseEntity<ReturnedSubmission> getSubmissionById(UUID submissionId) {
         return ResponseEntity.of(submissionRepository.findById(submissionId));
     }
 
@@ -76,8 +78,9 @@ public class SubmissionController implements SubmissionApi {
      * @return list of submissions. All submissions are returned if no criteria specified.
      */
     @Override
-    public ResponseEntity<List<Submission>> submissionGet(Long track, List<Long> author, List<String> keywords,
-                                                          SubmissionStatus status, String name) {
+    public ResponseEntity<List<ReturnedSubmission>> submissionGet(Long event, Long track,
+                      List<Long> author, List<String> keywords,
+                      SubmissionStatus status, String name) {
         return ResponseEntity.of(Optional.of(submissionRepository.findAll()));
     }
 
@@ -89,8 +92,8 @@ public class SubmissionController implements SubmissionApi {
      * @return response with updated submission if success, error otherwise
      */
     @Override
-    public ResponseEntity<Submission> submissionSubmissionIdPut(UUID submissionId,
-                                                                Submission updateSubmission) {
+    public ResponseEntity<ReturnedSubmission> submissionSubmissionIdPut(UUID submissionId,
+                                                                UpdateSubmission updateSubmission) {
         return submissionService.update(submissionId, updateSubmission);
     }
 }
--- submission-microservice/src/main/java/nl/tudelft/sem/template/submission/services/SubmissionService.java
+++ submission-microservice/src/main/java/nl/tudelft/sem/template/submission/services/SubmissionService.java
@@ -1,17 +1,21 @@
 package nl.tudelft.sem.template.submission.services;
 
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import nl.tudelft.sem.template.model.ReturnedSubmission;
 import nl.tudelft.sem.template.model.Submission;
+import nl.tudelft.sem.template.model.Track;
+import nl.tudelft.sem.template.model.UpdateSubmission;
 import nl.tudelft.sem.template.submission.repositories.SubmissionRepository;
-import nl.tudelft.sem.template.submission.services.TrackService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
 import org.springframework.web.bind.annotation.PathVariable;
+import com.fasterxml.jackson.core.type.TypeReference;
 
-import javax.sound.midi.Track;
-import javax.validation.constraints.Null;
+
 import java.time.LocalDateTime;
-import java.time.OffsetDateTime;
+import java.util.List;
 import java.util.Optional;
 import java.util.UUID;
 
@@ -23,15 +27,19 @@ public class SubmissionService {
 
     private final TrackService trackService;
 
+    private final HttpRequestService httpRequestService;
+
     /**
      * Submission Service constructor.
      *
      * @param submissionRepository submission repository
      */
     @Autowired
-    public SubmissionService(SubmissionRepository submissionRepository, TrackService trackService) {
+    public SubmissionService(SubmissionRepository submissionRepository, TrackService trackService,
+                             HttpRequestService httpRequestService) {
         this.submissionRepository = submissionRepository;
         this.trackService = trackService;
+        this.httpRequestService = httpRequestService;
     }
 
     /**
@@ -40,14 +48,25 @@ public class SubmissionService {
      * @param submission new submission
      * @return response with created submission if success, otherwise error
      */
-    public ResponseEntity<Submission> add(Submission submission) {
-        if (!trackService.checkSubmissionDeadline(submission.getTrackId())) {
-            return ResponseEntity.badRequest().build();
-        }
-        submission.setId(randomUUID());
-        submission.setCreated(LocalDateTime.now());
-        submission.setUpdated(submission.getCreated());
-        return ResponseEntity.ok(submissionRepository.save(submission));
+    public ResponseEntity<ReturnedSubmission> add(Submission submission) {
+        //if (checkDuplicateSubmissions(submission)) {
+        //if (!trackService.checkSubmissionDeadline(submission.getTrackId()))
+        //return ResponseEntity.badRequest().build();
+        //}
+        ReturnedSubmission returnedSubmission = new ReturnedSubmission(
+                submission.getTitle(),
+                submission.getAuthors(),
+                submission.getAbstract(),
+                submission.getFile(),
+                submission.getType(),
+                submission.getEventId(),
+                submission.getTrackId(),
+                submission.getSubmittedBy(),
+                UUID.randomUUID(),
+                LocalDateTime.now(),
+                LocalDateTime.now()
+        );
+        return ResponseEntity.ok(submissionRepository.save(returnedSubmission));
     }
 
     /**
@@ -57,7 +76,7 @@ public class SubmissionService {
      * @return response ok if submission is deleted, error otherwise
      */
     public ResponseEntity<Void> delete(@PathVariable("id") UUID submissionId) {
-        Optional<Submission> deleted = submissionRepository.findById(submissionId);
+        Optional<ReturnedSubmission> deleted = submissionRepository.findById(submissionId);
         if (deleted.isEmpty()) {
             return ResponseEntity.badRequest().build();
         }
@@ -72,15 +91,15 @@ public class SubmissionService {
      * @param updatedSubmission updated subission
      * @return response with updated submission if success, error otherwise
      */
-    public ResponseEntity<Submission> update(@PathVariable("id") UUID submissionId,
-                                             Submission updatedSubmission) {
+    public ResponseEntity<ReturnedSubmission> update(@PathVariable("id") UUID submissionId,
+                                             UpdateSubmission updatedSubmission) {
         return submissionRepository.findById(submissionId).map(submission -> {
-            submission.setStatus(updatedSubmission.getStatus());
             submission.setTitle(updatedSubmission.getTitle());
             submission.setAbstract(updatedSubmission.getAbstract());
             submission.setAuthors(updatedSubmission.getAuthors());
             submission.setKeywords(updatedSubmission.getKeywords());
             submission.setType(updatedSubmission.getType());
+            submission.setEventId(updatedSubmission.getEventId());
             submission.setTrackId(updatedSubmission.getTrackId());
             submission.setLink(updatedSubmission.getLink());
             submission.setUpdated(LocalDateTime.now());
@@ -89,6 +108,30 @@ public class SubmissionService {
 
         }).orElseGet(() -> ResponseEntity.badRequest().build());
 
+    }
+
+    /**
+     * This method will be used to check whether there are any submissions
+     * that are identical to the one that the user is trying to submit.
+     * To check for whether it is ideantical or not, we are just going to check for the title of the submission.
+     *
+     * @param submission a submission that we are trying to add
+     * @return boolean which returns ture if there are no identical submissions
+     */
+    public boolean checkDuplicateSubmissions(Submission submission) {
+        ObjectMapper objectMapper = new ObjectMapper();
+        String url = "http://ip_adress:8084/submission?title=" + submission.getTitle()
+                + "?eventId=" + submission.getEventId();
+
+        String receivedJson = httpRequestService.get(url).body();
+        List<Submission> submissions;
+        try {
+            submissions = objectMapper.readValue(receivedJson, new TypeReference<>() {});
+        } catch (JsonProcessingException e) {
+            throw new RuntimeException(e);
+        }
+
+        return submissions.isEmpty();
     }
 }
 
